2505561d33f395a1a860c79cc09f0af9a36d27fc
Asif Sardar <143050016+MrAsif2529@users.noreply.github.com>
2024-04-26 21:16:39 +0500

    Initial commit

---
+++ app/src/main/java/com/example/mylibrary/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mylibrary;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "Hello this is My Library", Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ my_library/src/main/java/com/example/my_library/Toaster.java
@@ -0,0 +1,5 @@
+package com.example.my_library;
+
+public class Toaster {
+    
+}
